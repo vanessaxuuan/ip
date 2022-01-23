3427794b32aca0c5dc05d8c9f6f8035b45e9771e
VanessaXuuan <vanessa@Vanessas-MacBook-Pro.local>
2022-01-23 19:14:34 +0800

    level2-gary

--- src/main/java/Gary.java
+++ src/main/java/Gary.java
@@ -1,15 +1,22 @@
+import java.sql.Array;
 import java.util.*;
 
 public class Gary {
     public static void main(String[] args) {
 
-        System.out.println("Hello, i'm Gary! What can I do for you?\n");
+        System.out.println("Hello, i'm Gary! What's on your to do list today?\n");
         Scanner sc = new Scanner(System.in);
+        ArrayList<String> items = new ArrayList<>();
         String nxt = sc.nextLine();
-        while(!nxt.equals("bye")) {
-            System.out.println(nxt); // echo
+        while (!nxt.equals("bye")) {
+            if (nxt.equals("list")) {
+                for (String str : items) {
+                    System.out.println(str);
+                }
+            }
+            items.add(nxt); // store
             nxt = sc.nextLine();
         }
-        System.out.println("Bye, have a nice day!\n");
+        System.out.println("Bye, hope you stay productive!\n");
     }
 }
