8f52035034ba1b34ab4d53964e6a0ef417acb7f7
vanessaxuuan <vanessakhor19@gmail.com>
2022-02-07 17:56:03 +0800

    add dates and times

--- src/main/java/Gary.java
+++ src/main/java/Gary.java
@@ -1,5 +1,11 @@
 import java.sql.Array;
-import java.util.*;
+import java.time.format.DateTimeParseException;
+import java.time.format.FormatStyle;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 
 public class Gary {
     public static void main(String[] args) throws GaryException {
@@ -67,15 +73,23 @@ public class Gary {
                             break;
                         case "event":
                             String[] e = nxt.split("/", 5);
-                            items.add(new Event(e[0].substring(6), e[1]));
+                            DateTimeFormatter inputFormat = DateTimeFormatter.ofPattern("dd-MM-yyyy,HHmm");
+                            LocalDateTime e1 = LocalDateTime.parse(e[1].strip(), inputFormat);
+                            DateTimeFormatter outputFormat = DateTimeFormatter.ofPattern("dd LLL yyyy HH:mm a");
+                            items.add(new Event(e[0].substring(6), e1.format(outputFormat)));
                             break;
                         case "deadline":
                             String[] d = nxt.split("/", 5);
-                            items.add(new Deadlines(d[0].substring(9), d[1]));
+                            DateTimeFormatter inFormat = DateTimeFormatter.ofPattern("dd-MM-yyyy,HHmm");
+                            LocalDateTime d1 = LocalDateTime.parse(d[1].strip(), inFormat);
+                            DateTimeFormatter outFormat = DateTimeFormatter.ofPattern("dd LLL yyyy HH:mm a");
+                            items.add(new Deadlines(d[0].substring(9), d1.format(outFormat)));
                             break;
                     }
                 } catch (StringIndexOutOfBoundsException e) {
-                    System.out.println("Ah please enter a valid description e.g. task_type name / date");
+                    System.out.println("Ah please enter a valid description e.g. task_type name / 19-01-2022,1530");
+                } catch (DateTimeParseException e) {
+                    System.out.println("Ah please enter a valid date e.g. 19-01-2022,2359");
                 }
             }
             nxt = sc.nextLine(); // continue getting inputs
