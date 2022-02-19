35b7840390852adbf927c8a6d146f6f9bff6ee19
vanessaxuuan <vanessakhor19@gmail.com>
2022-02-19 18:49:16 +0800

    Add GUI to Gary

---
+++ src/main/java/DialogBox.java
@@ -0,0 +1,67 @@
+package gary;
+
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.HBox;
+import javafx.scene.Node;
+import javafx.collections.ObservableList;
+import javafx.collections.FXCollections;
+import javafx.scene.shape.Circle;
+import javafx.scene.paint.Color;
+
+public class DialogBox extends HBox {
+
+    private Label text;
+    private ImageView displayPicture;
+
+    public DialogBox(Label l, ImageView iv) {
+        text = l;
+        displayPicture = iv;
+
+        // DialogBox background colour
+        Color shade = new Color(0.0,0.5,0.5,0.1);
+        CornerRadii space = new CornerRadii(5);
+        Insets offset = new Insets(5);
+        BackgroundFill fill = new BackgroundFill(shade, space, offset);
+        this.setBackground(new Background(fill));
+
+        // clip imageView to Circle
+        Circle clip = new Circle(50, 50,50);
+        displayPicture.setClip(clip);
+
+        text.setWrapText(true);
+        displayPicture.setFitWidth(100.0);
+        displayPicture.setFitHeight(100.0);
+
+        this.setAlignment(Pos.TOP_RIGHT);
+        this.getChildren().addAll(text, displayPicture);
+        this.setSpacing(10);
+        this.setPadding(new Insets(10));
+    }
+
+    /**
+     * Flips the dialog box such that the ImageView is on the left and text on the right.
+     */
+    private void flip() {
+        this.setAlignment(Pos.TOP_LEFT);
+        ObservableList<Node> tmp = FXCollections.observableArrayList(this.getChildren());
+        FXCollections.reverse(tmp);
+        this.getChildren().setAll(tmp);
+    }
+
+    public static DialogBox getUserDialog(Label l, ImageView iv) {
+        return new DialogBox(l, iv);
+    }
+
+    public static DialogBox getDukeDialog(Label l, ImageView iv) {
+        var db = new DialogBox(l, iv);
+        db.flip();
+        return db;
+    }
+}
+
---
+++ src/main/java/gary/Launcher.java
@@ -0,0 +1,9 @@
+package gary;
+import gary.Gary;
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(Gary.class, args);
+    }
+}
